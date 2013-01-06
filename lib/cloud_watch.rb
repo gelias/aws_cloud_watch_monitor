fb1b4aba5d476a432da80b89b4645b192279c850
Guilherme Elias <guilherme.elias@gmail.com>
2013-01-06 00:44:06 -0200

    Criado modulo para interação com EC2

--- lib/cloud_watch.rb
+++ lib/cloud_watch.rb
@@ -10,10 +10,24 @@ module CloudWatch
 
     def list
       resp = @cw.client.describe_alarms
-      resp[:metric_alarms].each do |alarm|
-      "#{alarm[:alarm_name]} : #{alarm[:state_value]}".to_s
-      end
+      resp[:metric_alarms]
+      #resp[:metric_alarms].each do |alarm|
+      #{}"#{alarm[:alarm_name]} : #{alarm[:state_value]}".to_s
+      #end
     end
 
   end
 end 
+
+module EC2 
+  class Instance
+    def initialize access_key, secret_key
+      AWS.config(:access_key_id => access_key,:secret_access_key => secret_key)
+      @instance = AWS::EC2::Instance.new('i-33ae7942')
+    end
+
+    def start
+      @instance.start
+    end
+  end
+end
--- spec/cloud_watch_spec.rb
+++ spec/cloud_watch_spec.rb
@@ -4,8 +4,22 @@ require '../lib/cloud_watch'
 
 describe "Alarm" do
   it "Should retrieve all alarms" do
-    alarm  = CloudWatch::Alarm.new '', ''
-    puts alarm.list
+    alarm  = CloudWatch::Alarm.new 'access_key', 'secret_key'
+    alarms = alarm.list
+    alarms.length.should > 0
   end
+end
+
+describe "EC2" do
+	it "Should stop ec2 instance" do
+		instance  = EC2::Instance.new 'access_key', 'secret_key'
+		instance.stop
+	end
+
+	it "Should stop ec2 instance" do
+		instance  = EC2::Instance.new 'access_key', 'secret_key'
+		instance.start
+	end
+
 end
 
